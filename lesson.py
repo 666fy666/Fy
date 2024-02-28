a379eec525355e01cbc6bb7bb6dc8cf2f0a67a86
fy <97347504+666fy666@users.noreply.github.com>
2024-02-28 21:03:29 +0800

    Update lesson.py

--- lesson.py
+++ lesson.py
@@ -40,49 +40,33 @@ class Lesson:
 
     def check_date(self):
         if self.week == 1:
-            if self.hour == 14 and self.minute == 30:
-                name = "载运工具运用工程"
-                local = "D314"
-                timing = "15:00"
-                return name, local, timing
-        if self.week == 2:
-            if self.hour == 9 and self.minute == 45:
-                name = "交通运输前沿课程"
-                local = "D214"
-                timing = "10.15"
-                return name, local, timing
-        if self.week == 3:
             if self.hour == 12 and self.minute == 50:
-                name = "交通运输工程学"
-                local = "D303"
+                name = "自然辩证法"
+                local = "J303"
                 timing = "13:20"
                 return name, local, timing
-            if self.hour == 14 and self.minute == 30:
-                name = "机器学习"
+        if self.week == 2:
+            if self.hour == 16 and self.minute == 5:
+                name = "艺术赏析"
+                local = "C402"
+                timing = "16:35"
+                return name, local, timing
+        if self.week == 3:
+            if self.hour == 7 and self.minute == 40:
+                name = "交通安全理论"
+                local = "A403"
+                timing = "13:20"
+                return name, local, timing
+            if self.hour == 17 and self.minute == 50:
+                name = "现代检测技术"
                 local = "C306"
-                timing = "15:00"
+                timing = "18:10"
                 return name, local, timing
         if self.week == 4:
             if self.hour == 9 and self.minute == 25:
-                name = "新思想"
-                local = "J302"
+                name = "英语"
+                local = "D306"
                 timing = "9:55"
-                return name, local, timing
-            if self.hour == 14 and self.minute == 30:
-                name = "英语课"
-                local = "D203或训2323"
-                timing = "15:00"
-                return name, local, timing
-        if self.week == 5:
-            if self.hour == 7 and self.minute == 45:
-                name = "矩阵论"
-                local = "F320"
-                timing = "8:15"
-                return name, local, timing
-            if self.hour == 12 and self.minute == 50:
-                name = "车辆动力学"
-                local = "C202"
-                timing = "13:20"
                 return name, local, timing
 
 
