15d541778f9eca46320a420e7cdd3496e8fb388b
fy <97347504+666fy666@users.noreply.github.com>
2024-03-17 13:19:52 +0800

    Update Weibo.py

--- Weibo.py
+++ Weibo.py
@@ -29,12 +29,12 @@ dbs = file.json()["db"]
 class WeiBo:
     def __init__(self, id):
         try:
-            db = pymysql.connect(host='%s' % host, user='%s' % user, password='%s' % pwd, port=3306, db='%s' % dbs)
+            db = pymysql.connect(host='192.168.66.239', user='%s' % user, password='%s' % pwd, port=3306, db='%s' % dbs)
             cursor = db.cursor()
             self.db = db
             self.cursor = cursor
         except:
-            db = pymysql.connect(host='192.168.66.239', user='%s' % user, password='%s' % pwd, port=3306, db='%s' % dbs)
+            db = pymysql.connect(host='%s' % host, user='%s' % user, password='%s' % pwd, port=3306, db='%s' % dbs)
             cursor = db.cursor()
             self.db = db
             self.cursor = cursor
